072415963c9aec5835e861e741bf91daf1ed028a
Robert Tanory <btanory@gmail.com>
2013-04-25 11:05:50 -0500

    Initial commit

---
+++ PatientPortal/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PatientPortal::Application.config.secret_token = '5a5d68756d08df31ecdabd0e3fa2574e99df12f1e98d2c64f98b566c8d63468c210714545def3920505e3b1388adb157082a681e0100e6350e9c8b15128c8016'

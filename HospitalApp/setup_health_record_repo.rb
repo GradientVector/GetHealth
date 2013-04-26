d2f43432bf24013c552ec2d07a8fd89d705f43cb
John Harvey <thegradientvector@gmail.com>
2013-04-26 11:39:51 -0500

    Changed location of health record repo init script.

--- HospitalApp/setup_health_record_repo.rb
+++ HospitalApp/setup_health_record_repo.rb
@@ -1,5 +1,6 @@
 `rm -r health_records/1`
 `git init health_records/1`
 `cp health_record.yml health_records/1/health_record.yml`
+Dir.chdir("health_records/1")
 `git add .`
 `git commit -m "Initial creation of health record."`
